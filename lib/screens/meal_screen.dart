8c0d2158801f08b20d80c811d9d1cff1c1203aa4
Md Mahadi Hasan <mahadi379377@gmail.com>
2025-02-13 16:12:25 +0600

    Create meal_screen.dart

---
+++ lib/screens/meal_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MealScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Meals')),
+      body: Center(child: Text('Meal Page')),
+    );
+  }
+}
