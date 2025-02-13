c13bc03e5a4a15b854711662dfc7799651f3af6e
Md Mahadi Hasan <mahadi379377@gmail.com>
2025-02-13 16:58:22 +0600

    Update meal_screen.dart

--- lib/screens/meal_screen.dart
+++ lib/screens/meal_screen.dart
@@ -1,11 +1,66 @@
+// lib/screens/meal_screen.dart
 import 'package:flutter/material.dart';
+import '../models/meal.dart';
+import '../services/meal_service.dart';
 
-class MealScreen extends StatelessWidget {
+class MealScreen extends StatefulWidget {
+  @override
+  _MealScreenState createState() => _MealScreenState();
+}
+
+class _MealScreenState extends State<MealScreen> {
+  final MealService _mealService = MealService();
+  List<Meal> _meals = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMeals();
+  }
+
+  Future<void> _fetchMeals() async {
+    List<Meal> meals = await _mealService.getMeals();
+    setState(() {
+      _meals = meals;
+    });
+  }
+
+  void _addMeal() {
+    // Code to add a new meal entry
+  }
+
+  void _generateMonthlyReport() {
+    // Code to generate monthly report
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Meals')),
-      body: Center(child: Text('Meal Page')),
+      body: ListView.builder(
+        itemCount: _meals.length,
+        itemBuilder: (context, index) {
+          final meal = _meals[index];
+          return ListTile(
+            title: Text('Date: ${meal.date}'),
+            subtitle: Text('Breakfast: ${meal.breakfast}, Lunch: ${meal.lunch}, Dinner: ${meal.dinner}'),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _addMeal,
+            child: Icon(Icons.add),
+          ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: _generateMonthlyReport,
+            child: Icon(Icons.bar_chart),
+          ),
+        ],
+      ),
     );
   }
 }
