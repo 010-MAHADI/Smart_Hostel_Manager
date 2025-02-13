40b0162b0e09274f4f51ddf3c3b6d01abb75ffa8
Md Mahadi Hasan <mahadi379377@gmail.com>
2025-02-13 16:25:17 +0600

    Create summary_screen2.dart

---
+++ lib/screens/summary_screen2.dart
@@ -0,0 +1,10 @@
+// lib/screens/summary_screen.dart
+class SummaryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Summary')),
+      body: Center(child: Text('Summary Page')),
+    );
+  }
+}
