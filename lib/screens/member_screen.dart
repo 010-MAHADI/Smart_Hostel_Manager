48b02ab5641864ba16b39c1c639a1bdac5acc6b3
Md Mahadi Hasan <mahadi379377@gmail.com>
2025-02-13 16:11:49 +0600

    Create member_screen.dart

---
+++ lib/screens/member_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MemberScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Members')),
+      body: Center(child: Text('Member List Page')),
+    );
+  }
+}
